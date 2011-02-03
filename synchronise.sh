f16388d63ac578af93b9231d866f1cd92d7ce70e
Matěj Laitl <matej@laitl.cz>
2011-02-03 12:42:53 +0100

    update synchronise.sh to be able to remove files

--- synchronise.sh
+++ synchronise.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-pushd ../pybayes
+PYBAYES_DIR="../pybayes"
+
+pushd "${PYBAYES_DIR}"
 rev=$(git describe)
 popd
 
-make -C ../pybayes/doc/ html
-cp -r ../pybayes/doc/_build/html/* ./
-git add .
-git commit -m "synchronise.sh: regenerate documentation for PyBayes ${rev}"
+make -C "${PYBAYES_DIR}/doc/" clean
+make -C "${PYBAYES_DIR}/doc/" html
+rsync -am -v --delete "${PYBAYES_DIR}/doc/_build/html/" \
+	--exclude /.git --exclude /synchronise.sh --exclude README \
+	--exclude /.nojekyll --exclude /.buildinfo \
+	./
+git commit -a -m "synchronise.sh: regenerate documentation for PyBayes ${rev}"
 git push
